40305d760275e5d64b484354d3404243606d3e88
Antoine Burnel <antoine.burnel3@gmail.com>
2022-01-27 22:24:27 +0100

    finition de la javadoc du package jeton

--- depot/puissance4/doc/package-search-index.js
+++ depot/puissance4/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"jeton"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"jeton"},{"l":"jeu"},{"l":"joueur"},{"l":"main"},{"l":"plateau"}];updateSearchResults();
