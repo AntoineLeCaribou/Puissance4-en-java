429703c9e74028ba0b82c43fce268b688ab92025
Antoine Burnel <antoine.burnel3@gmail.com>
2022-01-31 23:15:47 +0100

    Ajout de l'interface de jeu (non fonctionnelle)

---
+++ depot/puissance4/src/application/ControlleurJeu.java
@@ -0,0 +1,10 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class ControlleurJeu {
+
+	public void barreQuitter(ActionEvent e) {
+		ControlleurBarreSuperieur.quitter(e);
+	}
+}
--- depot/puissance4/src/application/ControlleurPrincipal.java
+++ depot/puissance4/src/application/ControlleurPrincipal.java
@@ -47,6 +47,19 @@ public class ControlleurPrincipal {
 		stage.show();
 	}
 	
+	private void sceneJeu(ActionEvent e) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("ChoixJoueur.fxml"));
+		root = loader.load();
+		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setTitle("Puissance 4 - Choix Des Joueurs");
+		ControlleurOptions controlleurOptions = loader.getController();
+		controlleurOptions.resetBoutonModeNuit();
+		controlleurOptions.setColorBG(Preferences.getColorModeNuit());
+		stage.show();
+	}
+	
 	public void setColorBG(Color couleur) {
 		if (couleur == Color.BLACK)
 			fond.setStyle("-fx-background-color: #333333");
