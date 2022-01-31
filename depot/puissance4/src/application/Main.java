5514d286d3b8971fa34cfc4b527a671175fb5073
Antoine Burnel <antoine.burnel3@gmail.com>
2022-01-31 21:08:57 +0100

    le menu principal (fxml) apparait desormais

--- depot/puissance4/src/application/Main.java
+++ depot/puissance4/src/application/Main.java
@@ -1,19 +1,34 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage stage) throws Exception {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root, 400, 400);
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			Parent root = FXMLLoader.load(getClass().getResource("Main_menu.fxml"));
+			
+			Scene scene = new Scene(root, Color.BLACK);
+			
+			Image icon = new Image("icon.png");
+			stage.getIcons().add(icon);
+			stage.setTitle("Puissance 4 - Main menu");
+			stage.setWidth(960);
+			stage.setHeight(540);
+			stage.setResizable(false);
+			//stage.setFullScreen(true);
+			//stage.setFullScreenExitHint("Appuyez sur 'Echap' pour réduire la fenêtre");
+			stage.setScene(scene);
+			stage.show();
+			
+			
 		}
 		catch(Exception e) {
 			e.printStackTrace();
